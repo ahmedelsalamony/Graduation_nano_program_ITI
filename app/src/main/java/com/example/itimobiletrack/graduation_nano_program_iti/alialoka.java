9c7688a1818c71112f800bd67f6a027d815e3d52
owner <aminahmed49@gmail.com>
2017-03-22 13:19:54 +0200

    update all amin

---
+++ app/src/main/java/com/example/itimobiletrack/graduation_nano_program_iti/alialoka.java
@@ -0,0 +1,14 @@
+package com.example.itimobiletrack.graduation_nano_program_iti;
+
+import android.widget.Toast;
+
+/**
+ * Created by owner on 3/22/2017.
+ */
+
+public class alialoka {
+    alialoka(){
+        System.out.println("go");
+
+    }
+}
