908868073dc6452baf7d7f96db03fa8818a24c7c
ahmed <ahmedabdelhameed79@gmail.com>
2017-03-30 13:55:32 +0200

    animation#2

--- app/src/main/java/com/example/itimobiletrack/graduation_nano_program_iti/Login/LoginRegisterActivity.java
+++ app/src/main/java/com/example/itimobiletrack/graduation_nano_program_iti/Login/LoginRegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.itimobiletrack.graduation_nano_program_iti.Login;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v4.app.FragmentManager;
@@ -9,12 +10,14 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.itimobiletrack.graduation_nano_program_iti.R;
 
 public class LoginRegisterActivity extends AppCompatActivity {
 
     private FragmentManager fm;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +32,14 @@ public class LoginRegisterActivity extends AppCompatActivity {
          fm = getSupportFragmentManager();
         // add
         FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.xPlaceHolder, new LoginFragment());
+        ft.add(R.id.xPlaceHolder, new LoginFragment());
         ft.addToBackStack(null);
-
         ft.commit();
 
 
     }
+
+
     public void toSignUp(View v){
         // replace
         FragmentTransaction ft = fm.beginTransaction();
@@ -51,11 +55,19 @@ public class LoginRegisterActivity extends AppCompatActivity {
         FragmentManager fm =getSupportFragmentManager();
         FragmentTransaction  ft=fm.beginTransaction();
         ft.replace(R.id.xPlaceHolder,forgetPass);
-        ft.addToBackStack(null);
+        //ft.addToBackStack(null);
         ft.commit();
 
     }
 
+    @Override
+    public void onBackPressed() {
 
-
+//-------------------following lines are to handle when press back button on
+// login fragment exit application ---------------------------//
+        Intent i=new Intent(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_HOME);
+        i.setFlags(i.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
 }
